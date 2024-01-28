6edf193994669d642cc26ee0ab7088a1f64e5a0e
Rodrigo Martins <rodrigolmti@gmail.com>
2024-01-28 15:48:30 -0600

    Bump app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
 
     defaultConfig {
         applicationId = "com.rodrigolmti.lunch.money.companion"
-        versionCode = 202401281
-        versionName = "1.1.0"
+        versionCode = 202401282
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
