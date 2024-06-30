bac4a156dbaa01fdcf812b61e81e903d1dd5edf9
Rodrigo M <rodrigo.martins@theboboapp.com>
2024-06-30 09:01:16 -0500

    Bump app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
 
     defaultConfig {
         applicationId = "com.rodrigolmti.lunch.money.companion"
-        versionCode = 329062024
-        versionName = "1.5.1"
+        versionCode = 330062024
+        versionName = "1.6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
